8522a45e71d6bfcfe0abd489261af9ca06e7fe7a
Valentin Marguerie <74249732+ValentinM27@users.noreply.github.com>
2022-04-21 22:00:28 +0200

    Init commit

---
+++ backend/database/migrations/2022_04_21_initialisation_database.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migration
+     * 
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->uuid('id')->unique();
+            $table->string('label', 30);
+            $table->timestamps();
+        });
+
+        Schema::create('suscribes', function (Blueprint $table) {
+            $table->uuid('id_category');
+            $table->uuid('id_user');
+
+            $table->foreign('id_category')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+
+            $table->unique(['id_category', 'id_user']);
+        });
+
+        Schema::create('posts', function (Blueprint $table) {
+            $table->uuid('id')->unique();
+            $table->string('content', 255);
+            $table->timestamps();
+
+            $table->uuid('id_category');
+            $table->uuid('id_user');
+
+            $table->foreign('id_category')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+        });
+
+        Schema::create('comments', function (Blueprint $table) {
+            $table->uuid('id')->unique();
+            $table->string('content', 255);
+            $table->timestamps();
+
+            $table->uuid('id_post');
+            $table->uuid('id_user');
+
+            $table->foreign('id_post')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+};
